febf3eedaa99cd018542e6c1ebcd235ebd996ac1
cforChobar <sidhuashman02@gmail.com>
2024-05-07 10:32:21 +0530

    first commit - created project, user model and message model

---
+++ src/model/User.ts
@@ -0,0 +1,74 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+//making the interface of message schema
+export interface Message extends Document {
+  content: string
+  createdAt: Date
+}
+
+//we have to provide our interface to this
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+})
+
+//making schema of user in same file
+export interface User extends Document {
+  username: string
+  email: string
+  password: string
+  verifyCode: string
+  verifyCodeExpiry: Date
+  isVerified: boolean
+  isAcceptingMessage: boolean
+  messages: Message[]
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please enter a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+})
+
+//while exporting the data in nextjs we have to make sure that our application is already booted up or not so use or condition because next js don't know if schema already exists or not so if it exists we are getting it otherwise creating the schema
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel;
---
+++ src/steps.js
@@ -0,0 +1,7 @@
+//Day first 7 May 2024
+/**
+1. Mastering ZOD 
+- created the next js project 
+- created models for varification and for backend using mongoose (User, Message)
+- instalL ZOD ---
+ */
