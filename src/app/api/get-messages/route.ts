6f4c27b445fd8fefae4666b4581371723b252ba1
cforChobar <sidhuashman02@gmail.com>
2024-05-18 09:03:22 +0530

    complete tutorial

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -6,7 +6,7 @@ import { User } from "next-auth"
 import mongoose from "mongoose"
 
 export async function GET(request: Request) {
-  dbConnect()
+  await dbConnect()
   const session = await getServerSession(authOptions)
   const user: User = session?.user as User
 
